18104352bc9885c9159e7fc7edffc80651790551
Junichi Ito <jit@sonicgarden.jp>
2017-09-30 09:14:58 +0900

    Add price method to taiyaki

--- taiyaki_test.rb
+++ taiyaki_test.rb
@@ -3,13 +3,27 @@ require 'minitest/autorun'
 class Taiyaki
   attr_reader :anko, :size
 
+  # 元の値段: 100円
+  # 白あん +30円
+  # 大きめ +50円
   def initialize(anko, size)
     @anko = anko
     @size = size
   end
 
+  def price
+    amount = 100
+    if anko == '白あん'
+      amount += 30
+    end
+    if size == '大きめ'
+      amount += 50
+    end
+    amount
+  end
+
   def to_s
-    "あんこ: #{anko}, 大きさ: #{size}"
+    "あんこ: #{anko}, 大きさ: #{size}, #{price}円"
   end
 end
 
@@ -18,11 +32,16 @@ class TaiyakiTest < Minitest::Test
     taiyaki_1 = Taiyaki.new('あずき', 'ふつう')
     assert_equal 'あずき', taiyaki_1.anko
     assert_equal 'ふつう', taiyaki_1.size
-    assert_equal 'あんこ: あずき, 大きさ: ふつう', taiyaki_1.to_s
+    assert_equal 100, taiyaki_1.price
+    assert_equal 'あんこ: あずき, 大きさ: ふつう, 100円', taiyaki_1.to_s
 
     taiyaki_2 = Taiyaki.new('白あん', '大きめ')
     assert_equal '白あん', taiyaki_2.anko
     assert_equal '大きめ', taiyaki_2.size
-    assert_equal 'あんこ: 白あん, 大きさ: 大きめ', taiyaki_2.to_s
+    assert_equal 180, taiyaki_2.price
+    assert_equal 'あんこ: 白あん, 大きさ: 大きめ, 180円', taiyaki_2.to_s
+
+    taiyaki_3 = Taiyaki.new('あずき', '大きめ')
+    assert_equal 150, taiyaki_3.price
   end
 end
